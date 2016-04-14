f60efc63e3615d42f83fe41a030432d8822892fe
gergling <gregmichaeldavies@gmail.com>
2016-04-14 15:43:10 +0100

    Test to drive instantiator development.

--- src/spec/unit/pulseball/factory/instantiator.js
+++ src/spec/unit/pulseball/factory/instantiator.js
@@ -14,10 +14,64 @@ describe('pulseballFactoryInstantiator', function () {
     });
 
     describe('the instance', function () {
+        var ranking = [
+            { "team": { "name": "Australia", "id": 32 }, "pos": 1, "pts": 54.23 },
+            { "team": { "name": "New Zealand", "id": 62 }, "pos": 2, "pts": 54.00 },
+            { "team": { "name": "France", "id": 2 }, "pos": 3, "pts": 52.95 },
+            { "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 52.32 },
+            { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }
+        ];
+
+        beforeEach(function () {
+            instance.init(ranking);
+        });
+
         describe('#addMatch(match)', function () {
             // Put in a match object
-            it('returns a ranking', function () {
+            var newRanking;
+            var expectedRanking = [
+                { "team": { "name": "Australia", "id": 32 }, "pos": 1, "pts": 54.23 },
+                { "team": { "name": "New Zealand", "id": 62 }, "pos": 2, "pts": 54.00 },
+                { "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 53.68 },
+                { "team": { "name": "France", "id": 2 }, "pos": 3, "pts": 51.59 },
+                { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }
+            ];
+            var match = {
+                "matchId": 2524,
+                "description": "Match 2",
+                "venue": {
+                    "id": 900,
+                    "name": "Stadium",
+                    "city": "Paris",
+                    "country": "France"
+                },
+                "teams": [
+                    {
+                        "id": 2,
+                        "name": "France",
+                        "abbreviation": "FRA"
+                    },
+                    {
+                        "id": 1,
+                        "name": "England",
+                        "abbreviation": "ENG"
+                    }
+                ],
+                "scores": [
+                    19,
+                    23
+                ],
+                "status": "C",
+                "outcome": "B"
+            };
+
+            beforeEach(function () {
+                newRanking = instance.addMatch(match);
+            });
+
+            it('returns the correct ranking', function () {
                 // Check returned object is of type ranking
+                expect(newRanking).toBe(expectedRanking);
             });
         });
     });
