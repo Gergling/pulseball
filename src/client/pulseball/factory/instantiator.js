cc68005c2375b7abe8ff8e37ce4bf2c6521f3775
gergling <gregmichaeldavies@gmail.com>
2016-04-15 00:24:26 +0100

    _winner function.

--- src/client/pulseball/factory/instantiator.js
+++ src/client/pulseball/factory/instantiator.js
@@ -1,6 +1,19 @@
 angular.module('pulseball').factory('pulseballFactoryInstantiator', function () {
     var PulseBall = function () {
         var rankings;
+
+        this._winner = function (match) {
+            var outcomes = {
+                A: 0,
+                B: 1,
+            };
+            var teamIDX = outcomes[match.outcome];
+            if (typeof teamIDX !== 'number') {
+                throw new Error('Looking for winner in a match which had no winners.')
+            }
+            return match.teams[teamIDX];            
+        };
+
         this.init = function (rankingsJson) {
             rankings = rankingsJson;
         };
