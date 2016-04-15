b23055a1bae189899bd73423ba23289ca1973fe5
gergling <gregmichaeldavies@gmail.com>
2016-04-15 17:42:50 +0100

    Removed superfluous whitespace.

--- src/client/pulseball/service/data.js
+++ src/client/pulseball/service/data.js
@@ -2,6 +2,6 @@ angular.module('pulseball').service('pulseballServiceData', function ($http) {
     var promise = $http.get('src/data/rankings.json').then(function (response) {
         PULSEBALL.init(response.data);
     });
-    
+
     this.promise = function () {return promise;};
 });
