58972aecee68b8872ba9d2b04ad518db086e97cc
gergling <gregmichaeldavies@gmail.com>
2016-04-15 02:11:42 +0100

    Initial commit of addMatch.

--- src/client/pulseball/factory/instantiator.js
+++ src/client/pulseball/factory/instantiator.js
@@ -14,7 +14,47 @@ angular.module('pulseball').factory('pulseballFactoryInstantiator', function () 
             rankings = rankingsJson;
         };
         this.addMatch = function (match) {
+            // Choose teams from rankings using match data
+            var teamRanks = match.teams.map(function (team, idx) {
+                return rankingTeam(team.id);
+            });
+            var differenceTenth = Math.min(10, Math.max(-10, teamRanks[0].pts - teamRanks[1].pts)) / 10;
+            var ratingChange;
+            switch (match.outcome) {
+                case 'A':
+                    ratingChange = 1 - differenceTenth;
+                    teamRanks[0].pts += ratingChange;
+                    teamRanks[1].pts -= ratingChange;
+                break;
+                case 'B':
+                    ratingChange = 1 + differenceTenth;
+                    teamRanks[0].pts -= ratingChange;
+                    teamRanks[1].pts += ratingChange;
+                break;
+                case 'D':
+                    teamRanks.forEach(function (teamRank) {
+                        teamRank.pts += differenceTenth;
+                    });
+                break;
+            }
+            //console.log(ratingChange, differenceTenth, teamRanks[0].score - teamRanks[1].score);
+            /*teamRanks.forEach(function (teamRank) {
+                rankingTeam(teamRank.team.id).pts = teamRank.pts;
+                console.log(teamRank.team.id);
+                console.log('before', rankingTeam(teamRank.team.id).pts);
+                console.log('after', teamRank.pts);
+            });*/
+
+            rankings = rankings.sort(function (a, b) {
+                //console.log(a.team.id, b.team.id, a.pts, b.pts);
+                return a.pts > b.pts;
+            });
             
+            rankings.forEach(function (x) {
+                console.log(x.team.id, x.pts);
+            });
+
+            return rankings;
         };
     };
 
