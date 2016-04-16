0eef16cce280a69527f6eccea5e473f4db57b7bb
gergling <gregmichaeldavies@gmail.com>
2016-04-16 02:36:48 +0100

    Remove superfluous data service.

--- src/client/pulseball/service/data.js
+++
@@ -1,7 +0,0 @@
-angular.module('pulseball').service('pulseballServiceData', function ($http) {
-    var promise = $http.get('src/data/rankings.json').then(function (response) {
-        PULSEBALL.init(response.data);
-    });
-
-    this.promise = function () {return promise;};
-});
